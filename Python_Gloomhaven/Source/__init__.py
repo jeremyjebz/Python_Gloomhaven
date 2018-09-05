a2080ce74fe119d1d2c7ae2d9a9e7880617c4979
jeremyjebz <jeremyjebz@gmail.com>
2018-09-04 20:18:41 -0500

    couple exercises

--- Python_Gloomhaven/Source/__init__.py
+++ Python_Gloomhaven/Source/__init__.py
@@ -1,1 +1,33 @@
-print("Hello World!")
+#
+# Data Types
+#
+print("***Data Types***")
+
+MyFloat = float(10)
+MyString = "hello"
+MyInt = 20
+
+print("Float: " + str(isinstance(MyFloat, float)))
+print("String: " + str(isinstance(MyString, str)))
+print("Int: " + str(isinstance(MyInt, int)))
+
+#
+# Lists
+#
+print("***Lists***")
+
+numbers = []
+strings = []
+names = ["Billy", "Bob", "John"]
+
+numbers.append(1)
+numbers.append(2)
+numbers.append(3)
+
+strings.append("One")
+strings.append("Two")
+strings.append("Three")
+
+print(numbers)
+print(strings)
+print(names[1])
